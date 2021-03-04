8e124ee20b4fdb1ed5f859f010276b69fe8fe4c3
Anne Fitz <afitz@esri.com>
2021-03-04 14:49:40 -0800

    initial ESM commit

---
+++ DS-2021/plenary/ESM/vite-arcgis-project/main.js
@@ -0,0 +1,16 @@
+import './style.css'
+import '@arcgis/core/assets/esri/themes/light/main.css';
+
+import WebMap from '@arcgis/core/WebMap';
+import MapView from '@arcgis/core/views/MapView';
+
+
+const map = new WebMap({
+  portalItem: {
+    id: "ceb8954a5f2c457284c5074efd5a5ca0"
+  }
+});
+const view = new MapView({
+  map: map,
+  container: "viewDiv"
+})
